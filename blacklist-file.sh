313618086c1744fdceec8e3f9825137e7da6d9f4
Shin Umeda <umeda.shin@gmail.com>
2024-04-10 18:51:59 -0700

    now case insensitive

--- blacklist-file.sh
+++ blacklist-file.sh
@@ -10,20 +10,20 @@ banlist=(
 for word in "${banlist[@]}"
 do
   if [ "$#" = 1 ]; then
-    grep -r $word $1 --exclude-dir='.git'
+    grep -r -iF "$word" $1 --exclude-dir='.git'
     if [  $? = 0 ]; then
       echo "Commit message found with $word"
       exit 1
     fi
   fi
 
-  grep -r $word . --exclude-dir='.git'
+  grep -r -iF "$word" . --exclude-dir='.git'
   if [ $? = 0 ]; then
     echo "File found with $word"
     exit 1
   fi
 
-  files=`find . -name "*$word*"  -not -path "./.git/*"`
+  files=`find . -iname "*$word*"  -not -path "./.git/*"`
   if [ ! -z "$files" ]; then
     echo "Filename found with $word"
     exit 1
