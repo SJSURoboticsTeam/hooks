62a8974f2af3ccc159efe1a523d8fffe221936ce
Shin Umeda <umeda.shin@gmail.com>
2024-04-10 18:40:53 -0700

    this also checks filenames

--- blacklist-file.sh
+++ blacklist-file.sh
@@ -10,16 +10,22 @@ banlist=(
 for word in "${banlist[@]}"
 do
   if [ "$#" = 1 ]; then
-    grep -r $word $1
+    grep -r $word $1 --exclude-dir='.git'
     if [  $? = 0 ]; then
-      exit 1 
+      echo "Commit message found with $word"
+      exit 1
     fi
   fi
 
-  grep -r $word .
+  grep -r $word . --exclude-dir='.git'
   if [ $? = 0 ]; then
+    echo "File found with $word"
     exit 1
   fi
 
-  
+  files=`find . -name "*$word*"  -not -path "./.git/*"`
+  if [ ! -z "$files" ]; then
+    echo "Filename found with $word"
+    exit 1
+  fi
 done
